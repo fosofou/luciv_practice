64756fb10b778069a993f795507c4a22b7ed5d16
Evgeny Shablakov <evgeny.shablakov@urfu.me>
2023-12-29 11:32:39 +0300

    test

--- 03_Fractal/fractal.h
+++ 03_Fractal/fractal.h
@@ -4,6 +4,9 @@
 #include "image.h"
 #include <assert.h>
 
+/**
+ * ... text ...
+ */
 void drawMandelbrot(imageP img, float left, float top, float xside, float yside);
 
 void drawSierpinskiTriangle(imageP img, int x1, int y1, int x2, int y2, int x3, int y3, int depth);
